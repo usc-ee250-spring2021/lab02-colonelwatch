a5529ab963b61eb5bae4fbbc181fd5b7292eba25
Kenny Peng <kennypen@usc.edu>
2021-02-05 01:43:27 +0000

    Completed assignment

--- ee250/lab02/grovepi_sensors.py
+++ ee250/lab02/grovepi_sensors.py
@@ -1,19 +1,10 @@
 """ EE 250L Lab 02: GrovePi Sensors
 
-List team members here.
+Kenny Peng.
 
-Insert Github repository link here.
+github.com/usc-ee250-spring2021/lab02-colonelwatch
 """
 
-"""python3 interpreters in Ubuntu (and other linux distros) will look in a 
-default set of directories for modules when a program tries to `import` one. 
-Examples of some default directories are (but not limited to):
-  /usr/lib/python3.5
-  /usr/local/lib/python3.5/dist-packages
-
-The `sys` module, however, is a builtin that is written in and compiled in C for
-performance. Because of this, you will not find this in the default directories.
-"""
 import sys
 import time
 # By appending the folder of all the GrovePi libraries to the system path here,
@@ -23,16 +14,24 @@ sys.path.append('../../Software/Python/')
 sys.path.append('../../Software/Python/grove_rgb_lcd')
 
 import grovepi
+import grove_rgb_lcd
 
-"""This if-statement checks if you are running this python file directly. That 
-is, if you run `python3 grovepi_sensors.py` in terminal, this if-statement will 
-be true"""
 if __name__ == '__main__':
-    PORT = 4    # D4
+    pot_port = 0 # A0
+    ultrasonic_port = 4    # D4
+
+    grove_rgb_lcd.setRGB(255, 255, 255)
 
     while True:
         #So we do not poll the sensors too quickly which may introduce noise,
         #sleep for a reasonable time of 200ms between each iteration.
         time.sleep(0.2)
 
-        print(grovepi.ultrasonicRead(PORT))
+        threshold = grovepi.analogRead(pot_port)
+        distance = grovepi.ultrasonicRead(ultrasonic_port)
+
+        if distance < threshold:
+            grove_rgb_lcd.setText_norefresh('%4dcm OBJ PRES\n%d' % (threshold, distance))
+        else:
+            grove_rgb_lcd.setText_norefresh('%4dcm         \n%d' % (threshold, distance))
+
